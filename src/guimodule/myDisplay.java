4d3aabab70bf319c894335f182bc9257af01a5f3
GulatiRohit <52568337+GulatiRohit@users.noreply.github.com>
2020-07-26 11:32:10 +0530

    Initial commit i.e, uploading all files

---
+++ src/guimodule/myDisplay.java
@@ -0,0 +1,26 @@
+package guimodule;
+import processing.core.PApplet;
+
+public class myDisplay extends PApplet
+{
+	
+	private static final long serialVersionUID = 1L;
+	public void setup()
+	{
+		size(400,400);
+		background(200,200,200);
+		
+	}
+	public void draw()
+	{
+		fill(255,255,0);
+		ellipse(200,200,390,390);
+		
+		fill(0,0,0);
+		ellipse(120,130,50,70);
+		ellipse(280,130,50,70);
+		
+		arc(200,280,100,100,0,PI);
+	}
+
+}
